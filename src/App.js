b025daa14b423f6cc298886275a55d232e50e053
ssw1994 <waghmare.sachin1994@rediffmail.com>
2022-09-15 21:37:17 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
-export function Box({ row,column,boxClick,text }) {
+export function Box({ row, column, boxClick, text }) {
   return (
-    <button className="box" onClick={()=>boxClick(row,column)}>
+    <button className="box" onClick={() => boxClick(row, column)}>
       {text}
     </button>
   );
@@ -10,55 +10,43 @@ export function Box({ row,column,boxClick,text }) {
 
 export default function App() {
   const players = {
-    player_1:'X',
-    player_2:'Y'
-  }
-  const [playerTurn,updatePlayerTurn] = useState(players.player_1);
+    player_1: 'X',
+    player_2: 'Y',
+  };
+  const [playerTurn, updatePlayerTurn] = useState(players.player_1);
 
-  const [gameBoard,updateBoard] = useState([
+  const [gameBoard, updateBoard] = useState([
     ['', '', ''],
     ['', '', ''],
     ['', '', ''],
   ]);
 
-  useEffect(()=>{
-      //logic checking winner losing or game is finished
+  useEffect(() => {
+    //logic checking winner losing or game is finished
+    //finished -> all items in the array are filled
+    // winner - >
+    // vertical
+    // horizontal
+    // diagonal check
+    // Player 1 =  X
+    // Player 2 = Y
+  }, [gameBoard]);
 
-      //finished -> all items in the array are filled
+  const togglePlayerTurn = () => {
+    updatePlayerTurn(
+      playerTurn === players.player_1 ? players.player_2 : players.player_1
+    );
+  };
 
-
-
-      // winner - >
-      // vertical
-      // horizontal
-      // diagonal check
-
-
-      // Player 1 =  X
-      // Player 2 = Y
-
-
-  },[gameBoard])
-
-  const togglePlayerTurn = ()=>{
-    updatePlayerTurn(playerTurn === players.player_1 ? players.player_2 : players.player_1)
-  }
-
-  const updateGameBoard=(row,column)=>{
-      const board = gameBoard;
-      if(board[row][column])return;
-      board[row][column] = playerTurn;
-      updateBoard([...board]);
-      togglePlayerTurn();
-  }
+  const updateGameBoard = (row, column) => {};
 
   return (
     <div>
-      {gameBoard.map((row,rowIndex) => {
+      {gameBoard.map((row, rowIndex) => {
         return (
           <div className="row">
-            {row.map((value,columnIndex) => {
-              return <Box row={rowIndex} column={columnIndex} boxClick={updateGameBoard} text={value}/>;
+            {row.map((value, columnIndex) => {
+              return <Box />;
             })}
           </div>
         );
@@ -66,4 +54,3 @@ export default function App() {
     </div>
   );
 }
-
