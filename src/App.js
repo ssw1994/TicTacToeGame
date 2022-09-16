d78479abe53a6b8bf46c300839b0f73ca6f6a3e5
ssw1994 <waghmare.sachin1994@rediffmail.com>
2022-09-16 21:02:30 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './style.css';
-export function Box({ row, column, boxClick, text }) {
+export function Box(props) {
+  const { row, column, boxClick, text } = props;
   return (
     <button className="box" onClick={() => boxClick(row, column)}>
       {text}
@@ -21,8 +22,54 @@ export default function App() {
     ['', '', ''],
   ]);
 
+  const checkGameFinished = () => {
+    for (let i = 0; i < gameBoard.length; i++) {
+      const rows = gameBoard[i];
+      const isValidRow = rows.every((ele) => {
+        return ele != '';
+      });
+      if (!isValidRow) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const verticalCheck = () => {
+    const [r1, r2, r3] = gameBoard;
+    console.log(r1, r2, r3);
+    for (let i = 0; i < 3; i++) {
+      if (r1[i] && r2[i] && r3[i] && r1[i] === r2[i] && r2[i] === r3[i]) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const horizontalCheck = () => {
+    const [r1, r2, r3] = gameBoard;
+    console.log(r1, r2, r3);
+    for (let i = 0; i < 3; i++) {
+      if (r1[i] && r2[i] && r3[i] && r1[i] === r2[i] && r2[i] === r3[i]) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const checkWinner = () => {
+    //vertical check
+    if (verticalCheck()) {
+      console.log('vertical winner');
+    }
+  };
+
   useEffect(() => {
     //logic checking winner losing or game is finished
+    if (checkGameFinished()) {
+      checkWinner();
+    }
+
     //finished -> all items in the array are filled
     // winner - >
     // vertical
@@ -38,7 +85,13 @@ export default function App() {
     );
   };
 
-  const updateGameBoard = (row, column) => {};
+  const updateGameBoard = (row, column) => {
+    const grid = gameBoard;
+    if (grid[row][column]) return;
+    grid[row][column] = playerTurn;
+    updateBoard([...grid]);
+    togglePlayerTurn();
+  };
 
   return (
     <div>
@@ -46,7 +99,14 @@ export default function App() {
         return (
           <div className="row">
             {row.map((value, columnIndex) => {
-              return <Box />;
+              return (
+                <Box
+                  boxClick={updateGameBoard}
+                  row={rowIndex}
+                  column={columnIndex}
+                  text={value}
+                />
+              );
             })}
           </div>
         );
